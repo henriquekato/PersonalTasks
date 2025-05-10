01214227c4270035c56b7e8063f8749c5559db6e
Shoji <hshojikato@gmail.com>
2025-05-10 15:01:35 -0300

    feat: add LocalDateConverter for dueDate conversion

--- app/src/main/java/com/example/personaltasks/model/TaskRoomDb.kt
+++ app/src/main/java/com/example/personaltasks/model/TaskRoomDb.kt
@@ -2,8 +2,10 @@ package com.example.personaltasks.model
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [Task::class], version = 1)
+@TypeConverters(LocalDateConverter::class)
 abstract class TaskRoomDb: RoomDatabase() {
     abstract fun taskDao(): TaskDAO
 }
