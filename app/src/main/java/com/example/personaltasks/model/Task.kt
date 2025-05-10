d0ffe24575a149754f24cc02090e9e78a21f3303
Shoji <hshojikato@gmail.com>
2025-05-10 13:01:00 -0300

    feat: convert Task class to data class and implement Parcelable

--- app/src/main/java/com/example/personaltasks/model/Task.kt
+++ app/src/main/java/com/example/personaltasks/model/Task.kt
@@ -1,7 +1,11 @@
 package com.example.personaltasks.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 
-class Task(var title: String, var description: String, var dueDate: LocalDate) {
+@Parcelize
+data class Task(var title: String, var description:
+String, var dueDate: LocalDate): Parcelable {
 
 }
