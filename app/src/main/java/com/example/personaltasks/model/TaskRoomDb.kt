f958c457ed6e708a0a2d0ffcb1560bfd1767c02c
Shoji <hshojikato@gmail.com>
2025-05-10 14:41:14 -0300

    feat: create TaskRoomDb class

---
+++ app/src/main/java/com/example/personaltasks/model/TaskRoomDb.kt
@@ -0,0 +1,9 @@
+package com.example.personaltasks.model
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskRoomDb: RoomDatabase() {
+    abstract fun taskDao(): TaskDAO
+}
