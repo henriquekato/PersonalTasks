15918592fef697f5ae5a274103ab81992589a4b5
Shoji <hshojikato@gmail.com>
2025-05-10 11:01:32 -0300

    feat: add toolbar to MainActivity and show options

--- app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.personaltasks.ui
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.example.personaltasks.R
 import com.example.personaltasks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        setSupportActionBar(amb.toolbarIn.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
