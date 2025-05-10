12afef6ba319eafbf448bcbe1a524cf557dd7eea
Shoji <hshojikato@gmail.com>
2025-05-10 10:40:17 -0300

    feat: activate view binding

--- app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.example.personaltasks.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.personaltasks.R
+import com.example.personaltasks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(amb.root)
     }
 }
