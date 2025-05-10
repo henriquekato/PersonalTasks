0e473ab8d7d4047269b0dcabbece87c101858628
Shoji <hshojikato@gmail.com>
2025-05-10 14:23:05 -0300

    feat: annotate Task class with Room entity and primary key

--- app/src/main/java/com/example/personaltasks/model/Task.kt
+++ app/src/main/java/com/example/personaltasks/model/Task.kt
@@ -1,11 +1,15 @@
 package com.example.personaltasks.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 
 @Parcelize
+@Entity
 data class Task(
+    @PrimaryKey(autoGenerate = true)
     var id: Int,
     var title: String,
     var description: String,
