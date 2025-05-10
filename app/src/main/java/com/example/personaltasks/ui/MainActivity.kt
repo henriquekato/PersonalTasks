982446df5983026dfd00d435bf4a6226a1a51e68
Shoji <hshojikato@gmail.com>
2025-05-10 11:13:14 -0300

    feat: implement option to navigate from MainActivity to TaskActivity

--- app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.personaltasks.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.personaltasks.R
 import com.example.personaltasks.databinding.ActivityMainBinding
@@ -21,5 +23,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.add_task_mi -> {
+                startActivity(Intent(this, TaskActivity::class.java))
+                true
+            }
+            else -> {
+                false
+            }
+        }
     }
 }
