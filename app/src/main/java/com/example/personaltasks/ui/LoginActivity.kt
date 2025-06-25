06c37d0268b4637320b6766573866955f2ad0394
Shoji <hshojikato@gmail.com>
2025-06-24 22:55:09 -0300

    feat: implement login

--- app/src/main/java/com/example/personaltasks/ui/LoginActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.example.personaltasks.ui
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.personaltasks.R
 import com.example.personaltasks.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
     private val alb: ActivityLoginBinding by lazy {
@@ -19,5 +22,33 @@ class LoginActivity : AppCompatActivity() {
 
         setSupportActionBar(alb.toolbarIn.toolbar)
         supportActionBar?.subtitle = "Login"
+
+        alb.signUpBt.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        alb.signInBt.setOnClickListener {
+            val signinCoroutineScope = CoroutineScope(Dispatchers.IO)
+            signinCoroutineScope.launch {
+                Firebase.auth.signInWithEmailAndPassword(
+                    alb.emailLoginEt.text.toString(),
+                    alb.passwordLoginEt.text.toString()
+                ).addOnCompleteListener{
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Logged in successfully",
+                        Toast.LENGTH_SHORT
+                    )
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                    finish()
+                }.addOnFailureListener {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Login failed",
+                        Toast.LENGTH_SHORT
+                    )
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/personaltasks/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/RegisterActivity.kt
@@ -2,11 +2,7 @@ package com.example.personaltasks.ui
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.personaltasks.R
 import com.example.personaltasks.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
