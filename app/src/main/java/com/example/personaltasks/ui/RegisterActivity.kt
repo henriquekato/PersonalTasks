78644770cdd2e5d676c0ee42463dfc3a7a7d9655
Shoji <hshojikato@gmail.com>
2025-06-24 22:17:04 -0300

    feat: add register activity

---
+++ app/src/main/java/com/example/personaltasks/ui/RegisterActivity.kt
@@ -0,0 +1,23 @@
+package com.example.personaltasks.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.personaltasks.R
+import com.example.personaltasks.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private val arb: ActivityRegisterBinding by lazy {
+        ActivityRegisterBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(arb.root)
+
+        setSupportActionBar(arb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = "Register"
+    }
+}
