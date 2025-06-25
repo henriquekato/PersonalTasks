9f6adfc85052b6ac3280aed9b09da544c1d1801d
Shoji <hshojikato@gmail.com>
2025-06-24 22:15:46 -0300

    feat: add login activity

---
+++ app/src/main/java/com/example/personaltasks/ui/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.personaltasks.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.personaltasks.R
+import com.example.personaltasks.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private val alb: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(alb.root)
+
+        setSupportActionBar(alb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = "Login"
+    }
+}
